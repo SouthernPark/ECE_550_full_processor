d4932b72f1e53ff0ad48a7dde347389d11c34cb4
SouthernPark <2848919007@qq.com>
2021-11-18 17:24:20 -0500

    remove unnecessary file

--- mux_2.v
+++
@@ -1,9 +0,0 @@
-module mux_2(a, b, sel, out);
-
-    input [31:0] a, b;
-    input sel;
-    output [31:0] out;
-    
-    assign out = sel ? b : a;
-
-endmodule
--- mux_2_1bit.v
+++
@@ -1,9 +0,0 @@
-module mux_2_1bit(a, b, sel, out);
-
-    input a, b;
-    input sel;
-    output out;
-    
-    assign out = sel ? b : a;
-
-endmodule
